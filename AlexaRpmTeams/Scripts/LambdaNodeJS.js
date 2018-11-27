b73a01003b015c01522659ea9254e030c98847b0
kamrul <ozdreamer.it@gmail.com>
2018-11-27 18:21:51 +1000

    Little change in the lambda node.js

---
+++ AlexaRpmTeams/Scripts/LambdaNodeJS.js
@@ -0,0 +1,114 @@
+﻿
+'use strict';
+
+// --------------- Helpers that build all of the responses -----------------------
+
+function buildSpeechletResponse(title, output, repromptText, shouldEndSession) {
+    return {
+        outputSpeech: {
+            type: 'PlainText',
+            text: output,
+        },
+        card: {
+            type: 'Simple',
+            title: `SessionSpeechlet - ${title}`,
+            content: `SessionSpeechlet - ${output}`,
+        },
+        reprompt: {
+            outputSpeech: {
+                type: 'PlainText',
+                text: repromptText,
+            },
+        },
+        shouldEndSession,
+    };
+}
+
+function buildResponse(sessionAttributes, speechletResponse) {
+    return {
+        version: '1.0',
+        sessionAttributes,
+        response: speechletResponse,
+    };
+}
+
+
+// --------------- Functions that control the skill's behavior -----------------------
+
+function getWelcomeResponse(callback) {
+    // If we wanted to initialize the session to have some attributes we could add those here.
+    const sessionAttributes = {};
+    const cardTitle = 'Welcome';
+    const speechOutput = 'Hi Kamrul, I am Alexa. How can I help?';
+    const repromptText = 'Sorry I did not understand. Please repeat the question';
+    const shouldEndSession = false;
+
+    callback(sessionAttributes,
+        buildSpeechletResponse(cardTitle, speechOutput, repromptText, shouldEndSession));
+}
+
+function handleSessionEndRequest(callback) {
+    const cardTitle = 'Session Ended';
+    const speechOutput = 'Thank you Kamrul. Have a nice day!';
+    // Setting this to true ends the session and exits the skill.
+    const shouldEndSession = true;
+
+    callback({}, buildSpeechletResponse(cardTitle, speechOutput, null, shouldEndSession));
+}
+
+// --------------- Events -----------------------
+
+function onLaunch(launchRequest, session, callback) {
+    getWelcomeResponse(callback);
+}
+
+function onIntent(intentRequest, session, callback) {
+    const intent = intentRequest.intent;
+    const intentName = intentRequest.intent.name;
+    let speechOutput = '';
+    let repromptText = '';
+    let sessionAttributes = {};
+    
+    // Dispatch to your skill's intent handlers
+    if (intentName === 'Intent') {
+        speechOutput = 'Speech';
+    } else if (intentName === 'AMAZON.StopIntent' || intentName === 'AMAZON.CancelIntent') {
+        handleSessionEndRequest(callback);
+    } else {
+        repromptText = 'Sorry I did not understand. Please repeat the question';
+    }
+    
+    if (speechOutput != '') {
+        callback(sessionAttributes, buildSpeechletResponse(intent.name, speechOutput, repromptText, false));
+    }
+}
+
+function onSessionEnded(sessionEndedRequest, session) {
+    console.log(`onSessionEnded requestId=${sessionEndedRequest.requestId}, sessionId=${session.sessionId}`);
+}
+
+
+// --------------- Main handler -----------------------
+
+exports.handler = (event, context, callback) => {
+    try {
+        if (event.request.type === 'LaunchRequest') {
+            onLaunch(event.request,
+                event.session,
+                (sessionAttributes, speechletResponse) => {
+                    callback(null, buildResponse(sessionAttributes, speechletResponse));
+                });
+        } else if (event.request.type === 'IntentRequest') {
+            onIntent(event.request,
+                event.session,
+                (sessionAttributes, speechletResponse) => {
+                    callback(null, buildResponse(sessionAttributes, speechletResponse));
+                });
+        } else if (event.request.type === 'SessionEndedRequest') {
+            onSessionEnded(event.request, event.session);
+            callback();
+        }
+    } catch (err) {
+        callback(err);
+    }
+};
